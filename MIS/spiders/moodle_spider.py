205859c6f31c7dc40fa29d92f9dab0d342054c15
Kanishk Singh <singh.kanishk07@gmail.com>
2017-10-03 23:58:28 +0530

    Improved xpath scraping workflow. Also moved back to ConfigParser.
    I'll be running the script on my Raspberry Pi for now.

--- MIS/spiders/moodle_spider.py
+++ MIS/spiders/moodle_spider.py
@@ -1,47 +1,36 @@
+#!/usr/local/bin/python2.7
 import os
+import ConfigParser
 from scrapy.spiders.init import InitSpider
 from scrapy.http import Request, FormRequest
 from ..items import LecturesItem, PracticalsItem
 from scrapy.shell import inspect_response
 from scrapy.crawler import CrawlerProcess
 
-USERNAME = os.environ['USERNAME']
-PASSWORD = os.environ['PASSWORD']
+config = ConfigParser.RawConfigParser()
+config.read('./MIS/spiders/creds.ini')
+USERNAME = config.get('MIS', 'USERNAME')
+PASSWORD = config.get('MIS', 'PASSWORD')
 
-xpath = {
-    #Lectures
-    'AM_pcent': '//table[1]/tr[3]/td[4]/text()',
-    'AM_pcent_red' : '//table[1]/tr[3]/td[4]/font/u/b/text()',
-    'AP_pcent': '//table[1]/tr[4]/td[4]/text()',
-    'AP_pcent_red' : '//table[1]/tr[4]/td[4]/font/u/b/text()',
-    'AC_pcent' : '//table[1]/tr[5]/td[4]/text()',
-    'AC_pcent_red': '//table[1]/tr[5]/td[4]/font/u/b/text()',
-    'EM_pcent' : '//table[1]/tr[6]/td[4]/text()',
-    'EM_pcent_red': '//table[1]/tr[6]/td[4]/font/u/b/text()',
-    'BEE_pcent' : '//table[1]/tr[7]/td[4]/text()',
-    'BEE_pcent_red': '//table[1]/tr[7]/td[4]/font/u/b/text()',
-    'EVS_pcent' : '//table[1]/tr[8]/td[4]/text()',
-    'EVS_pcent_red': '//table[1]/tr[8]/td[4]/font/u/b/text()',
-    'Overall' : '//center/h2/text()',
-    'Overall_red': '//center/h2/u/b/text()',
-    'total_lec_conducted' : '//table[1]/tr[9]/td[2]/b/text()',
-    'total_lec_attended' : '//table[1]/tr[9]/td[3]/b/text()',
-    #Practicals
-    'AC_prac': '//table[2]/tr[2]/td[4]/text()',
-    'AC_prac_red' : '//table[2]/tr[2]/td[4]/font/u/b/text()',
-    'AM_prac' : '//table[2]/tr[3]/td[4]/text()',
-    'AM_prac_red': '//table[2]/tr[3]/td[4]/font/u/b/text()',
-    'AP_prac' : '//table[2]/tr[4]/td[4]/text()',
-    'AP_prac_red' : '//table[2]/tr[4]/td[4]/font/u/b/text()',
-    'BEE_prac': '//table[2]/tr[5]/td[4]/text()',
-    'BEE_prac_red': '//table[2]/tr[5]/td[4]/font/u/b/text()',
-    'Workshop': '//table[2]/tr[6]/td[4]/text()',
-    'Workshop_red': '//table[2]/tr[6]/td[4]/font/u/b/text()',
-    'EM_prac' : '//table[2]/tr[7]/td[4]/text()',
-    'EM_prac_red' : '//table[2]/tr[7]/td[4]/font/u/b/text()',
-    'Overall_prac': '//label/h2/text()',
-    'Overall_prac_red' : '//label/h2/b/text()'
-}
+
+xpaths=[
+    {"name": "AM", "query": "//table[1]/tr[3]/td[4]/", "check_for_red": True, "is_practical":False},
+    {"name": "AP", "query": "//table[1]/tr[4]/td[4]/", "check_for_red": True, "is_practical":False},
+    {"name": "AC", "query": "//table[1]/tr[5]/td[4]/", "check_for_red": True, "is_practical":False},
+    {"name": "EM", "query": "//table[1]/tr[6]/td[4]/", "check_for_red": True, "is_practical":False},
+    {"name": "BEE", "query": "//table[1]/tr[7]/td[4]/", "check_for_red": True, "is_practical":False},
+    {"name": "EVS", "query": "//table[1]/tr[8]/td[4]/", "check_for_red": True, "is_practical":False},
+    {"name": "Overall", "query": "//center/h2/", "check_for_red": True, "is_practical":False},
+    {"name": "total_lec_conducted", "query": "//table[1]/tr[9]/td[2]/b/text()", "check_for_red":False, "is_practical":False},
+    {"name": "total_lec_attended", "query": "//table[1]/tr[9]/td[3]/b/text()", "check_for_red": False, "is_practical":False},
+    {"name": "AC_prac", "query": "//table[2]/tr[2]/td[4]/", "check_for_red": True, "is_practical":True},
+    {"name": "AM_prac", "query": "//table[2]/tr[3]/td[4]/", "check_for_red": True, "is_practical":True},
+    {"name": "AP_prac", "query": "//table[2]/tr[4]/td[4]/", "check_for_red": True, "is_practical":True},
+    {"name": "BEE_prac", "query": "//table[2]/tr[5]/td[4]/", "check_for_red": True, "is_practical":True},
+    {"name": "Workshop", "query": "//table[2]/tr[6]/td[4]/", "check_for_red": True, "is_practical":True},
+    {"name": "EM_prac", "query": "//table[2]/tr[7]/td[4]/", "check_for_red": True, "is_practical":True},
+    {"name": "Overall_prac", "query": "//label/h2/", "check_for_red": True, "is_practical":True}
+]
 
 class MySpider(InitSpider):
     name = 'mis'
@@ -76,98 +65,28 @@ class MySpider(InitSpider):
         '''Scrape attendance data from page'''
         #inspect_response(response, self) #for debugging
 
-        #LECTURES
-        if response.xpath(xpath['AM_pcent']).extract()[0].strip() == "":
-            AM = response.xpath(xpath['AM_pcent_red']).extract()[0].strip()
-        else:
-            AM = response.xpath(xpath['AM_pcent']).extract()[0].strip()
-        
-        if response.xpath(xpath['AP_pcent']).extract()[0].strip() == "":
-            AP = response.xpath(xpath['AP_pcent_red']).extract()[0].strip()
-        else:
-            AP = response.xpath(xpath['AP_pcent']).extract()[0].strip()
-        
-        if response.xpath(xpath['AC_pcent']).extract()[0].strip() == "":
-            AC = response.xpath(xpath['AC_pcent_red']).extract()[0].strip()
-        else:
-            AC = response.xpath(xpath['AC_pcent']).extract()[0].strip()
-        
-        if response.xpath(xpath['EM_pcent']).extract()[0].strip() == "":
-            EM = response.xpath(xpath['EM_pcent_red']).extract()[0].strip()
-        else:
-            EM = response.xpath(xpath['EM_pcent']).extract()[0].strip()
-        
-        if response.xpath(xpath['BEE_pcent']).extract()[0].strip() == "":
-            BEE = response.xpath(xpath['BEE_pcent_red']).extract()[0].strip()
-        else:
-            BEE = response.xpath(xpath['BEE_pcent']).extract()[0].strip()
-        
-        if response.xpath(xpath['EVS_pcent']).extract()[0].strip() == "":
-            EVS = response.xpath(xpath['EVS_pcent_red']).extract()[0].strip()
-        else:
-            EVS = response.xpath(xpath['EVS_pcent']).extract()[0].strip()
-        
-        if response.xpath(xpath['Overall']).extract()[0].strip() == "Overall Attendance:":
-            Overall = response.xpath(xpath['Overall_red']).extract()[0].strip()
-        else:
-            Overall = response.xpath(xpath['Overall']).extract()[0].strip()
-        
-        #PRACTICALS
-        if response.xpath(xpath['AC_prac']).extract()[0].strip() == "":
-            AC_prac = response.xpath(xpath['AC_prac_red']).extract()[0].strip()
-        else:
-            AC_prac = response.xpath(xpath['AC_prac']).extract()[0].strip()
-        
-        if response.xpath(xpath['AM_prac']).extract()[0].strip() == "":
-            AM_prac = response.xpath(xpath['AM_prac_red']).extract()[0].strip()
-        else:
-            AM_prac = response.xpath(xpath['AM_prac']).extract()[0].strip()
-        
-        if response.xpath(xpath['AP_prac']).extract()[0].strip() == "":
-            AP_prac = response.xpath(xpath['AP_prac_red']).extract()[0].strip()
-        else:
-            AP_prac = response.xpath(xpath['AP_prac']).extract()[0].strip()
-        
-        if response.xpath(xpath['BEE_prac']).extract()[0].strip() == "":
-            BEE_prac = response.xpath(xpath['BEE_prac_red']).extract()[0].strip()
-        else:
-            BEE_prac = response.xpath(xpath['BEE_prac']).extract()[0].strip()
-        
-        if response.xpath(xpath['Workshop']).extract()[0].strip() == "":
-            Workshop = response.xpath(xpath['Workshop_red']).extract()[0].strip()
-        else:
-            Workshop = response.xpath(xpath['Workshop']).extract()[0].strip()
-        
-        if response.xpath(xpath['EM_prac']).extract()[0].strip() == "":
-            EM_prac = response.xpath(xpath['EM_prac_red']).extract()[0].strip()
-        else:
-            EM_prac = response.xpath(xpath['EM_prac']).extract()[0].strip()
-        
-        if response.xpath(xpath['Overall_prac']).extract()[0].strip() == "Overall Practical Attendance:":
-            Overall_prac = response.xpath(xpath['Overall_prac_red']).extract()[0].strip()
-        else:
-            Overall_prac = str(response.xpath(xpath['Overall_prac']).extract()).strip()[33:-2]
-        
-        
+        lecture_kwargs = {}
+        practicals_kwargs = {}
 
-        yield LecturesItem(
-            AM=AM,
-            AP=AP,
-            AC=AC,
-            EM=EM,
-            BEE=BEE,
-            EVS=EVS,
-            Overall=Overall,
-            total_lec_conducted=response.xpath(xpath['total_lec_conducted']).extract()[0].strip(),
-            total_lec_attended=response.xpath(xpath['total_lec_attended']).extract()[0].strip()
-        )
+        for xpath in xpaths:
+            query = xpath['query']
+            if xpath["check_for_red"]:
+                query = xpath['query'] + "text()"
+                query2 = xpath['query'] + "font/u/b/text()"
+                query3 = xpath['query'] + "u/b/text()"
 
-        yield PracticalsItem(
-            AC_prac=AC_prac,
-            AM_prac=AM_prac,
-            AP_prac=AP_prac,
-            BEE_prac=BEE_prac,
-            Workshop=Workshop,
-            EM_prac=EM_prac,
-            Overall_prac=Overall_prac,
-        )
+                value1 = response.xpath(query).extract_first()
+                value2 = response.xpath(query2).extract_first()
+                value3 = response.xpath(query3).extract_first()
+                value = value3 or value2 or value1
+            else:
+                value = response.xpath(query).extract_first()
+
+            if xpath["is_practical"]:
+                practicals_kwargs[xpath["name"]] = value.strip()
+            else:
+                lecture_kwargs[xpath["name"]] = value
+
+        yield LecturesItem(**lecture_kwargs)
+
+        yield PracticalsItem(**practicals_kwargs)

04d87f1d6d449eb3497bf99cf87c011f35016219
Kanishk Singh <singh.kanishk07@gmail.com>
2017-10-04 00:03:15 +0530

    Added until80 function.

--- MIS/mis_misc_functions.py
+++ MIS/mis_misc_functions.py
@@ -2,18 +2,18 @@ from __future__ import division
 import json
 from collections import Counter
 from operator import sub
+from sympy.solvers import solve
+from sympy import Symbol, Eq, solveset
 def bunk_lecture(n, tot_lec):
     with open('../attendance_output.json', 'r') as f:
         report = json.loads(f.read())
     result = (((int(report[0]['total_lec_attended']) + int(tot_lec)) -  int(n))/(int(report[0]['total_lec_conducted']) + tot_lec)) * 100
     return round(result, 2) #Round up to 2 decimals.
 
-'''def difference():
-    with open('./attendance_output.json', 'r') as f:
-        d1 = json.loads(f.read())
-    with open('../old_report.json', 'r') as o:
-        d2 = json.loads(o.read())
-    
-    return d3'''
-
-#print (difference())
+def until80():
+	with open('../attendance_output.json', 'r') as f:
+		report = json.loads(f.read())
+	x = Symbol('x')
+	expr = Eq((((int(report[0]['total_lec_attended']) + x)/(int(report[0]['total_lec_conducted']) + x))*100), 80)
+	soln = solveset(expr, x)
+	return next(iter(soln)) # Extracting the integer from singleton set soln.

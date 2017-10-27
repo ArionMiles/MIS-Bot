7de59d20bbfec5bfbf0d2dce93725460f8ee661f
Kanishk Singh <ArionMiles@users.noreply.github.com>
2017-10-28 00:46:52 +0530

    Relocated certain files (#9)
    * Fixed until80 function.
    
    * Fixed file paths
    
    * Fixed file paths
    
    * Moved init_db() to main()
    
    thanks to Arush15June :D
    
    * Added docstrings and some code cleanup.
    
    * Added help function. Amended start, register. Fixed other minor issues.
    
    * Relocated certain files & minor fixes.

--- mis_functions.py
+++ mis_functions.py
@@ -5,13 +5,13 @@ from operator import sub
 from sympy.solvers import solve
 from sympy import Symbol, Eq, solveset
 def bunk_lecture(n, tot_lec):
-    with open('./attendance_output.json', 'r') as f:
+    with open('attendance_output.json', 'r') as f:
         report = json.loads(f.read())
     result = (((int(report[0]['total_lec_attended']) + int(tot_lec)) -  int(n))/(int(report[0]['total_lec_conducted']) + tot_lec)) * 100
     return round(result, 2) #Round up to 2 decimals.
 
 def until80():
-	with open('./attendance_output.json', 'r') as f:
+	with open('attendance_output.json', 'r') as f:
 		report = json.loads(f.read())
 	x = Symbol('x')
 	expr = Eq((((int(report[0]['total_lec_attended']) + x)/(int(report[0]['total_lec_conducted']) + x))*100), 80)

dffd27a5c4020346decd631ab9aa0d381f0ff110
Kanishk Singh <singh.kanishk07@gmail.com>
2017-10-30 21:07:18 +0530

    space

--- telegram_bot.py
+++ telegram_bot.py
@@ -232,7 +232,7 @@ def help(bot, update):
 
 def tips(bot, update):
     tips = ["Always use /attendance command before using /until80 or /bunk to get latest figures.",\
-    "The Aldel MIS gets updated at 6PM everyday.", "The /until80 function gives you the number of \
+    "The Aldel MIS gets updated at 6PM everyday.", "The /until80 function gives you the number of\
     lectures you must attend *consecutively* before you attendance is 80%."]
     messageContent = random.choice(tips)
     bot.sendMessage(chat_id=update.message.chat_id, text=messageContent)

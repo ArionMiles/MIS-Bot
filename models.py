0f02e8a6301c9dde267f2b3fa0279ee66d943976
charge <ahujaarush@gmail.com>
2017-11-03 23:06:08 +0530

    added Attendance, Subject models

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from datetime import datetime
 from database import Base
 
 class Chat(Base):
@@ -7,6 +8,7 @@ class Chat(Base):
     PID = Column(String(100))
     password = Column(String(50))
     chatID = Column(String(512))
+    subjects = relationship('Subject', backref = 'chats')   
 
     def __init__(self, PID, password, chatID):
         self.PID = PID
@@ -15,3 +17,33 @@ class Chat(Base):
 
     def __repr__(self):
         return '<Chat {} chatID: {}>'.format(self.PID, self.chatID)
+
+
+class Subject(Base):
+    __tablename__ = 'subjects'
+    id = Column(Integer, primary_key = True)
+    chat_id = Column(Integer, ForeignKey('Chat.id'))
+    name = Column(String(100))  
+    attendance = relationship('Attendance', backref = 'subjects')
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def __repr__(self):
+        return '<Subject {} chatID: {}>'.format(self.name, self.chatID)
+
+class Attendance(Base):
+    __tablename__ = 'subjects'
+    id = Column(Integer, primary_key = True)
+    subject_id = Column(Integer, ForeignKey('Subject.id'))
+    date = Column(DateTime, default=datetime.utcnow)
+    value = Column(Integer, default=0)
+
+    def __init__(self, value, date=datetime.utcnow()):
+        self.value = value
+        self.date = date
+
+    def __repr__(self):
+        return '<Attendance {} Date: {} >'.format(self.value, self.date)
+
+    

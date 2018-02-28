312cfa00af7a7e484c80c86bc3a7b2b37b1535fd
Kanishk Singh <singh.kanishk07@gmail.com>
2018-02-28 20:42:52 +0530

    Removed unused dependencies and added docstrings

--- mis-bot/mis_functions.py
+++ mis-bot/mis_functions.py
@@ -1,16 +1,13 @@
 from __future__ import division
-import json
-from collections import Counter
-from operator import sub
 from sympy.solvers import solve
 from sympy import Symbol, Eq, solveset
+import requests
 
 from scraper.database import init_db, db_session
 from scraper.models import Attendance
 
-import requests
-
 def bunk_lecture(n, tot_lec, chatID):
+    '''Bunk calculator'''
     init_db()
     record = db_session.query(Attendance).filter(Attendance.chatID == chatID).first()
     attended = record.total_lec_attended
@@ -19,6 +16,7 @@ def bunk_lecture(n, tot_lec, chatID):
     return round(result, 2) #Round up to 2 decimals.
 
 def until80(chatID):
+    '''Calculates the no. of lectures user must attend in order to get attendance to 80%'''
     init_db()
     record = db_session.query(Attendance).filter(Attendance.chatID == chatID).first()
     attended = record.total_lec_attended
@@ -29,6 +27,7 @@ def until80(chatID):
     return next(iter(soln)) # Extracting the integer from singleton set soln.
 
 def check_login(username, password):
+    '''Checks if user input for their credentials is correct.'''
     base_url = 'http://report.aldel.org/student_page.php'
     payload = {
         'studentid':username,

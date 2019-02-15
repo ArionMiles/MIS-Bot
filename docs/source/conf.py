190ac2cf89b0832e9ae063595dc3fb4476774bd1
Kanishk Singh <singh.kanishk07@gmail.com>
2019-02-15 20:57:16 +0530

    Changed docs theme to sphinx_rtd_theme and added avatar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,13 +75,22 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '../../media/avatar.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '../../media/avatar-ico.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

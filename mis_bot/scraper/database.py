9262dad14237d57a3817a199f9a8b04371de9607
Kanishk Singh <singh.kanishk07@gmail.com>
2020-01-15 19:35:35 +0530

    Allow sharing db connection across threads

--- mis_bot/scraper/database.py
+++ mis_bot/scraper/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.pool import StaticPool
 
 # Database
-engine = create_engine('sqlite:///files/chats.db', convert_unicode=True)
+engine = create_engine('sqlite:///files/chats.db', convert_unicode=True, 
+                        connect_args= {'check_same_thread': False},
+                        poolclass=StaticPool)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                         autoflush=False,
                                         bind=engine))
